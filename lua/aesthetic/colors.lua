cf7bfa609a4005bad0ab9e34ee2dcb196f39dbf4
jadnw <jadenwu137@protonmail.com>
2022-12-08 10:02:32 +0700

    fix: change param color

--- lua/aesthetic/colors.lua
+++ lua/aesthetic/colors.lua
@@ -118,7 +118,7 @@ function M.gen_spec(accent, pal)
     keyword = pal.purple.base, -- Keywords
     number = pal.orange.bright, -- Numbers
     operator = pal.purple.bright, -- Operators
-    param = pal.orange.bright, -- Params
+    param = spec.fg1, -- Params
     preproc = pal.purple.base, -- PreProc
     regex = pal.orange.base, -- Regex
     statement = pal.purple.base, -- Statements
